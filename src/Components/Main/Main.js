1bb3938061f9381395f32c076fe0e9f27c097438
Alexsandr Ryabow <morboaport.ru@mail.ru>
2022-02-18 01:20:46 +0400

    V-01

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,36 +1,44 @@
-import React, {Component} from 'react';
-import './Main.scss';
+import React from "react";
+import Button from "../Button/Button";
+import styles from './Main.module.scss';
+import landMark from '../../icons/Landmark.svg';
+import burger from '../../icons/burger.svg';
 
-const Main = ({}) =>{
+const Main = () =>{
     return(
-    <div className='mainPage'>
-        <div className='titlePage'>
-            <div className='sideLine'>
-                <div className='burgerMenu'>
-
+    <div className = {styles.mainPage}>
+        <div className={styles.titlePage}>
+            <div className={styles.sideLine}>
+                <div className={styles.burgerMenu}>
+                    <img src={burger} alt="burger"> 
+                    </img>
                 </div>
-                <div className='languageButton'>
-                    <p>ENG</p>
+                <div className={styles.languageButton}>
+                    <span>Eng</span>
                 </div>
             </div>
-            <div className='mainBody'>
-                <div className='bodyTop'>
-                    <p> Need for drive</p>
-                    <div className='cityPick'>
-                        <img>
+            <div className= {styles.mainBody}>
+                <div className= {styles.bodyTop}>
+                    <span className={styles.motto}> Need for drive</span>
+                    <div className= {styles.cityPick}>
+                        <img src={landMark} alt="landMark" >
                         </img>
-                        <p className='cityName'> Ульяновск</p>
+                        <p className={styles.cityName}> Ульяновск</p>
                     </div>
                 </div>
-                <div className='bodyMiddle'>
-
+                <div className={styles.bodyMiddle}>
+                    <span className={styles.middleTopText} >Каршеринг</span>
+                    <span className={styles.middleText} >Need for drive</span>
+                    <span className={styles.middleBottomText} >Поминутная аренда авто твоего города</span>
+                    <Button></Button> 
                 </div>
-                <div className='bodyBottom'>
-
+                <div className={styles.bodyBottom}>
+                    <span className={styles.tradeMark}>© 2016-2019 «Need for drive»</span>
+                    <span className={styles.phone}>8 (495) 234-22-44</span>
                 </div>
             </div>
         </div>
-        <div className='slider'>
+        <div className={styles.slider}>
 
         </div>
     </div>
